62daeccc8df45d9b12facbe994664d15b34c4c84
Nabeel Khan <nabeel.kay@gmail.com>
2023-06-18 15:11:01 +0100

    wip

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+
+  const onClick=()=>{
+    
+  }
+  return (
+    <header className='header'>
+<h1>{title}</h1>
+<Button color='green' text='Add' onClick={onClick}/>
+
+    </header>
+  )
+}
+
+export default Header
